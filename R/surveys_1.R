1e351372660ce342175ba0a5972e4d610b339aa6
victoria <victoria.rodriguez-3@selu.edu>
2021-11-24 15:43:46 +0000

    Getting rid of NA values from surveys data.

--- R/surveys_1.R
+++ R/surveys_1.R
@@ -1,4 +1,20 @@
+#' Getting rid of NA values 
+#' 
+#' @param surveys is data
+#' @param surveys_1 is data without NA values 
+#' @return surveys_1 the product of getting rid of NA values from surveys data
+
+
 surveys_1 <- surveys %>%
 na.omit(surveys)
+  na.omit(surveys)
 surveys_1
+
+is.na(surveys_1) == TRUE) {
+  return("Yep you got NA's in this data") 
+}
+return(surveys_1)
+
+
+
 
