4734be8960417512e26b51b1a5ba65c4ff640859
victoria <victoria.rodriguez-3@selu.edu>
2021-11-24 18:26:32 +0000

    How to load in surveys data and test function.

--- R/surveys_data.R
+++ R/surveys_data.R
@@ -1,3 +1,17 @@
+#' Load in surveys data
+#' 
+#' @param surveys is data
+#' @return surveys data that is a data frame
+
+
 download.file(url="https://ndownloader.figshare.com/files/2292169", destfile = "data/surveys.csv")
 surveys <- read.csv("data/surveys.csv")
 surveys
+
+is.data.frame(surveys) == FALSE ) {
+  return("ERROR - object is not a data frame")
+return(surveys)
+}
+
+
+
