2d9ccae62f861a12f70076e5546dc4f8d4153aa4
victoria <victoria.rodriguez-3@selu.edu>
2021-11-24 18:44:47 +0000

    Statistical test with linear plot test.

--- R/Statistical_LinearPlot.R
+++ R/Statistical_LinearPlot.R
@@ -1,3 +1,13 @@
+#' Point plot and statistical test using surveys data with out NA values which is surveys_1
+#'
+#' @param weight a column in surveys_1 data
+#' @param genus a column in surveys_1 data
+#' @param year a column in surveys_1 data 
+#' @param surveys_1 data without NA values 
+#' @return surveys_1 
+
+
+
 ggplot(data = surveys_1, mapping = aes(x = year, y = weight)) + geom_point()+ facet_wrap(vars(genus))+ labs(title = "Observed weight of genera through time", x = "Year of observation", y = "Weight of individuals") + theme_bw() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), text = element_text(size = 20)) + stat_smooth()
 
 model_fit <- lm(weight ~ year, data = surveys_1)
