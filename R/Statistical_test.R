e9d5df0fad22ebec407acf73cde0272f0c5aeae1
victoria <victoria.rodriguez-3@selu.edu>
2021-11-30 22:38:53 +0000

    Statistical test function.

---
+++ R/Statistical_test.R
@@ -0,0 +1,18 @@
+#' Statistical test using data with out NA values 
+#'
+#' @param data is the data frame without NA's  
+#' @param weight is a column in the data frame
+#' @param year is a column in the data frame
+#' @return product is a summary of model_fit 
+
+
+
+Test <- function(data, weight, year){
+ model_fit <- lm(weight ~ year, data = data)
+ product <- summary(model_fit) 
+ if (is.list(product) == FALSE ) {
+ return("ERROR - object is not a data frame")
+}
+  return(product)
+}
+
