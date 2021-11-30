e6261ca40f0641c183a21139a8d31c651b1156f6
victoria <victoria.rodriguez-3@selu.edu>
2021-11-30 22:37:47 +0000

    Pointplot function and test.

--- R/PointPlot.R
+++ R/PointPlot.R
@@ -1,23 +1,15 @@
-#' Point plot using surveys data with out NA values which is surveys_1
+#' Point plot using data with out NA values 
 #'
-#' @param weight a column in surveys_1 data
-#' @param genus a column in surveys_1 data
-#' @param year a column in surveys_1 data 
-#' @param surveys_1 data without NA values 
-#' @return surveys_1 
+#' @param data is the data frame without NA's
+#' @return product is a point plot with x as year and y as weight 
 
 
-surveys_1
-weight <- surveys_1[9]
-weight
-genus <- surveys_1[10]
-genus
-year <- surveys_1[4]
-year
-ggplot(data = surveys_1, mapping = aes(x = year, y = weight)) + geom_point()+ facet_wrap(vars(genus))+ labs(title = "Observed weight of genera through time", x = "Year of observation", y = "Weight of individuals") + theme_bw() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), text = element_text(size = 20))
 
 
-is.Coord(surveys_1) == FALSE) {
-  return("ERROR - object is not a coordinare system") 
+Pointplot <- function(data){
+ product <- ggplot(data = data, mapping = aes(x = year, y = weight)) + geom_point()+ facet_wrap(vars(genus))+ labs(title = "Observed weight of genera through time", x = "Year of observation", y = "Weight of individuals") + theme_bw() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), text = element_text(size = 20))
+ if (is.ggplot(product) == FALSE) {
+ return("ERROR - object is not a coordinare system") 
 }
-return(surveys_1)
+return(product)
+}
