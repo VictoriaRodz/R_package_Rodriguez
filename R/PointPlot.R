1ee7f7084b0b73c9fd1beb1a8a16f4276fefc01c
victoria <victoria.rodriguez-3@selu.edu>
2021-12-01 20:34:13 +0000

    Pointplot function and test.

--- R/PointPlot.R
+++ R/PointPlot.R
@@ -9,7 +9,7 @@
 Pointplot <- function(data){
  product <- ggplot(data = data, mapping = aes(x = year, y = weight)) + geom_point()+ facet_wrap(vars(genus))+ labs(title = "Observed weight of genera through time", x = "Year of observation", y = "Weight of individuals") + theme_bw() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), text = element_text(size = 20))
  if (is.ggplot(product) == FALSE) {
- return("ERROR - object is not a coordinare system") 
+ return("ERROR - x is not a ggplot object") 
 }
 return(product)
 }
